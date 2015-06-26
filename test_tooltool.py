9d62b16bf522925565677c239f41ef0aa346104d
Ralph Giles <giles@mozilla.com>
2015-06-26 07:15:17 -0700

    Add zipfile test coverage.

--- test_tooltool.py
+++ test_tooltool.py
@@ -1212,9 +1212,17 @@ class FetchTests(TestDirMixin, unittest.TestCase):
         self.setup_tarball('tar -cjf basename.tar.bz2 basename')
         self.try_untar_file('basename.tar.bz2')
 
+    def test_untar_file_zip(self):
+        self.setup_tarball('zip -qr basename.zip basename')
+        self.try_untar_file('basename.zip')
+
     def test_untar_file_invalid_xz(self):
         self.setup_tarball('echo BOGUS > basename.tar.xz')
         self.assertFalse(tooltool.untar_file('basename.tar.xz'))
+
+    def test_untar_file_invalid_zip(self):
+        self.setup_tarball('echo BOGUS > basename.zip')
+        self.assertFalse(tooltool.untar_file('basename.zip'))
 
     def test_untar_file_not_tarfile(self):
         open('basename.tar.shrink', 'w').write('not a tarfile')
